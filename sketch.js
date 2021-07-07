58ef98d5af6a3262dbbc2b92ef12722a393bd571
RatnaNimmagadda14 <86669765+RatnaNimmagadda14@users.noreply.github.com>
2021-07-07 17:21:05 +0430

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+if(keyDown("left")){
+background("yellow")
+
+
+}
+if(keyDown("right")){
+background("pink")
+}
+if(keyDown("up"))
+background("brown")
+if(keyDown("down"))
+background("peach")
+}
+
+
+
+
